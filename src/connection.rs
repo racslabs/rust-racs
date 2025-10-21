96c737d38be493c9a0674a069f8b12472e3a8417
corbinmoon <clangmoon@gmail.com>
2025-10-21 11:53:59 -0400

    Update connection.rs

--- src/connection.rs
+++ src/connection.rs
@@ -1,9 +1,9 @@
 use std::collections::VecDeque;
-use std::net::{ TcpStream };
+use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
+use std::io::{Read, Write, Result};
 
-
-struct ConnectionPool {
+pub struct ConnectionPool {
     addr: String,
     pool: Arc<Mutex<VecDeque<TcpStream>>>,
     size: usize
@@ -39,4 +39,24 @@ impl ConnectionPool {
         pool.drain(..);
     }
 
+}
+
+pub fn send(stream: &mut TcpStream, data: &[u8]) -> Result<Vec<u8>> {
+    let prefix = (data.len() as u64).to_le_bytes();
+
+    let mut message = Vec::with_capacity(8 + data.len());
+    message.extend_from_slice(&prefix);
+    message.extend_from_slice(data);
+
+    stream.write_all(&message)?;
+    stream.flush()?;
+
+    let mut buf = [0u8; 8];
+    stream.read_exact(&mut buf)?;
+    let len = u64::from_le_bytes(buf);
+
+    let mut buf = vec![0u8; len as usize];
+    stream.read_exact(&mut buf)?;
+
+    Ok(buf)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,20 @@ pub mod connection;
 
 #[cfg(test)]
 mod tests {
+    use crate::connection::{send, ConnectionPool};
     use super::*;
 
     #[test]
     fn it_works() {
-        println!("test");
+        let pool = ConnectionPool::new("localhost:6381", 1);
+        let mut socket = pool.get().unwrap();
+
+        let res = send(&mut socket, "ls\'*\'\0".as_bytes()).expect("panic message");
+
+        let s = String::from_utf8_lossy(&res);
+        println!("{}", s);
+
+        pool.put(socket);
+        pool.close();
     }
 }
